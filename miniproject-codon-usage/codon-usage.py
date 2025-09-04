4a5e12122827a04f7d01aca03e45349756e7ccd5
Zhiyu Zheng <zhengzy1223@gmail.com>
2025-09-04 14:52:13 -0400

    Allow sequence comparison

--- miniproject-codon-usage/codon-usage.py
+++ miniproject-codon-usage/codon-usage.py
@@ -4,22 +4,40 @@ import sys
 import fasta
 import codons
 
-aas = dict()
+aas1 = dict()
+aas2 = dict()
 
 my_file = open(sys.argv[1])
-contigs = fasta.FASTAReader(my_file)
+your_file = open(sys.argv[2])
+contigs1 = fasta.FASTAReader(my_file)
+contigs2 = fasta.FASTAReader(your_file)
 
-for ident, sequence in contigs:
+for ident, sequence in contigs1:
     # print(ident)
     for i in range(0,len(sequence),3):
         codon = sequence[i:i+3]
         # print(codon)
         aa = codons.forward[codon]
-        if aa in aas:
-            aas[aa] += 1
+        if aa in aas1:
+            aas1[aa] += 1
         else:
-            aas[aa] = 1
+            aas1[aa] = 1
+
+for ident, sequence in contigs2:
+    for i in range(0,len(sequence),3):
+        codon = sequence[i:i+3]
+        aa = codons.forward[codon]
+        if aa in aas2:
+            aas2[aa] += 1
+        else:
+            aas2[aa] = 1
+
+alpha_aa = sorted(list(codons.reverse.keys()))
 
 my_file.close()
+your_file.close()
 
-print(aas)
+for aa in alpha_aa:
+    print(f"{aa}\t{aas1[aa]}\t{aas2[aa]}")
+
+# print(aas)
